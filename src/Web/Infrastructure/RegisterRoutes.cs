b503770c97354332692d4803b3011c2c4029a261
hazzik <hazzik@gmail.com>
2012-04-19 00:10:53 +0600

    Setup MvcExtensions

---
+++ src/Web/Infrastructure/RegisterRoutes.cs
@@ -0,0 +1,21 @@
+﻿namespace Web.Infrastructure
+{
+    using System.Web.Mvc;
+    using System.Web.Routing;
+    using MvcExtensions;
+
+    public class RegisterRoutes : RegisterRoutesBase
+    {
+        public RegisterRoutes(RouteCollection routes) : base(routes)
+        {
+        }
+
+        protected override void Register()
+        {
+            Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            Routes.MapRoute("Default", "{controller}/{action}/{id}",
+                            new {controller = "Home", action = "Index", id = UrlParameter.Optional});
+        }
+    }
+}
