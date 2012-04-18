589b314128da5d318e8bf55b6d4137f43e394b27
hazzik <hazzik@gmail.com>
2012-04-19 00:05:03 +0600

    Add application project

---
+++ src/Application/Home/HomeController.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Home
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
