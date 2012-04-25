2083e24eb7c23af5f24f1150712e8ef2620c3657
hazzik <hazzik@gmail.com>
2012-04-25 23:55:18 +0600

    Add FluentMigrator Add Migrations Assembly Add MigrateUp task

--- default.ps1
+++ default.ps1
@@ -1,5 +1,18 @@
+Framework '4.0'
+
+properties {
+	$configuration = 'Debug'
+	$platform = 'Any CPU'
+}
+
 Task Default -Depends Build
 
 Task Build {
-    msbuild
-} 
+	MSBuild	-t:Build -p:Configuration=$configuration
+}
+
+Task MigrateUp -Depends Build {
+	$fluentmigrator = Get-Item "packages/FluentMigrator.Tools.*/tools/AnyCPU/40/Migrate.exe"
+	$migrationsAssembly = Get-Item "src/Migrations/bin/$configuration/*Migrations.dll"
+	Invoke-Expression "$fluentmigrator --provider=sqlserver --connectionString=Main --target=$migrationsAssembly"
+}
---
+++ src/Migrations/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Migrations")]
+[assembly: Guid("8c9d1fce-92c0-416f-a5ba-fa20c04239b2")]
