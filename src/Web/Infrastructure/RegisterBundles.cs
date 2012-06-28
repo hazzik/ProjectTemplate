c2640d1a22eef0d051b6b49df29f577c3fc94dc9
hazzik <hazzik@gmail.com>
2012-06-29 00:13:18 +0600

    Update packages

--- src/Web/Infrastructure/RegisterBundles.cs
+++ src/Web/Infrastructure/RegisterBundles.cs
@@ -3,6 +3,7 @@
     using System.Web.Optimization;
     using BundleTransformer.Core.Orderers;
     using BundleTransformer.Core.Transformers;
+    using BundleTransformer.Core.Translators;
     using BundleTransformer.Less.Translators;
     using BundleTransformer.Yui.Minifiers;
     using MvcExtensions;
@@ -11,37 +12,33 @@
     {
         public override TaskContinuation Execute()
         {
-            var cssTransformer = new CssTransformer(new YuiCssMinifier(), new[] {new LessTranslator()});
-            cssTransformer.ForceReleaseMode();
+            var cssTransformer = new CssTransformer(new YuiCssMinifier(), new ITranslator[] {new LessTranslator()});
             var jsTransformer = new JsTransformer(new YuiJsMinifier());
-            jsTransformer.ForceReleaseMode();
             var nullOrderer = new NullOrderer();
 
-            var commonStylesBundle = new Bundle("~/CommonStyles", cssTransformer);
-            commonStylesBundle.AddFile("~/Content/site.less");
-            commonStylesBundle.AddFile("~/Content/themes/base/jquery-ui.css");
-            commonStylesBundle.AddFile("~/Content/bootstrap.min.css");
-            commonStylesBundle.AddFile("~/Content/bootstrap-responsive.min.css");
-            commonStylesBundle.Orderer = nullOrderer;
+            BundleTable.Bundles.Add(new Bundle("~/CommonStyles", cssTransformer)
+                {
+                    Orderer = nullOrderer
+                }.Include("~/Content/site.less",
+                          "~/Content/themes/base/jquery-ui.css",
+                          "~/Content/bootstrap.min.css",
+                          "~/Content/bootstrap-responsive.min.css"));
 
-            BundleTable.Bundles.Add(commonStylesBundle);
+            BundleTable.Bundles.Add(new Bundle("~/Modernizr", jsTransformer)
+                {
+                    Orderer = nullOrderer
+                }.Include("~/Scripts/modernizr-2.5.3.js"));
 
-            var modernizrBundle = new Bundle("~/Modernizr", jsTransformer);
-            modernizrBundle.AddFile("~/Scripts/modernizr-2.5.3.js");
-            modernizrBundle.Orderer = nullOrderer;
+            BundleTable.Bundles.Add(new Bundle("~/CommonScripts", jsTransformer)
+                {
+                    Orderer = nullOrderer
+                }.Include("~/Scripts/jquery-1.7.2.min.js",
+                          "~/Scripts/jquery-ui-1.8.20.min.js",
+                          "~/Scripts/jquery.validate.min.js",
+                          "~/Scripts/jquery.validate.unobtrusive.min.js",
+                          "~/Scripts/jquery.unobtrusive-ajax.min.js",
+                          "~/Scripts/bootstrap.min.js"));
 
-            BundleTable.Bundles.Add(modernizrBundle);
-
-            var commonScriptsBundle = new Bundle("~/CommonScripts", jsTransformer);
-            commonScriptsBundle.AddFile("~/Scripts/jquery-1.7.2.min.js");
-            commonScriptsBundle.AddFile("~/Scripts/jquery-ui-1.8.19.min.js");
-            commonScriptsBundle.AddFile("~/Scripts/jquery.validate.min.js");
-            commonScriptsBundle.AddFile("~/Scripts/jquery.validate.unobtrusive.min.js");
-            commonScriptsBundle.AddFile("~/Scripts/jquery.unobtrusive-ajax.min.js");
-            commonScriptsBundle.AddFile("~/Scripts/bootstrap.min.js");
-            commonScriptsBundle.Orderer = nullOrderer;
-
-            BundleTable.Bundles.Add(commonScriptsBundle);
             return TaskContinuation.Continue;
         }
     }
