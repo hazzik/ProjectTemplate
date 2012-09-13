45082212461e822c65bd79654640acfaeead547e
Alexander I. Zaytsev <hazzik@gmail.com>
2012-09-13 17:44:23 +0600

    Add packaging with MsDeploy of default Web project

--- default.ps1
+++ default.ps1
@@ -1,16 +1,29 @@
 Framework '4.0'
 
 properties {
+    $home = $psake.build_script_dir
 	$configuration = 'Debug'
 	$platform = 'Any CPU'
 	$fluentmigrator = Get-Item "packages/FluentMigrator.Tools.*/tools/AnyCPU/40/Migrate.exe"
 	$xunitconsole = Get-Item "packages/xunit.runners.*/tools/xunit.console.clr4.exe"
+    $packageLocation = "$home/artifacts/WebPackage.zip"
+    $buildTempDirectory = "$home/build"
+    $tempPackageDirectory = "$buildTempDirectory/temp/" + [System.Guid]::NewGuid().ToString("N")
 }
 
 Task Default -Depends Build
 
+Task Init {
+    [IO.Directory]::CreateDirectory($packageLocation)
+}
+
 Task Build {
-	MSBuild	-t:Build -p:Configuration=$configuration
+	MSBuild -t:Build -p:Configuration=$configuration
+}
+
+Task Package -Depends Build {
+    MSBuild -t:Package -p:Configuration=$configuration -p:PackageLocation=$packageLocation -p:_PackageTempDir=$tempPackageDirectory src/Web/Web.csproj
+    Remove-Item -Recurse -Force $buildTempDirectory
 }
 
 Task Test -Depends Build {
